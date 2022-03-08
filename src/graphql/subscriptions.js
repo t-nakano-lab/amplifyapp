95bfce828cb2f1301dbcd52bc8e0a68181e7d8ea
中野 敏成 <t-nakano@ARSAD.LAN>
2022-03-08 13:47:10 +0900

    added strage

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,48 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateNote = /* GraphQL */ `
+  subscription OnCreateNote {
+    onCreateNote {
+      id
+      name
+      description
+      image
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
+export const onUpdateNote = /* GraphQL */ `
+  subscription OnUpdateNote {
+    onUpdateNote {
+      id
+      name
+      description
+      image
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
+export const onDeleteNote = /* GraphQL */ `
+  subscription OnDeleteNote {
+    onDeleteNote {
+      id
+      name
+      description
+      image
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
