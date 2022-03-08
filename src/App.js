c41f9a237443f4da2b1ecd51b8fa5f277933fe0f
中野 敏成 <t-nakano@ARSAD.LAN>
2022-03-08 14:00:31 +0900

    added strage

--- src/App.js
+++ src/App.js
@@ -111,24 +111,22 @@ async function fetchNotes() {
       />
       <button onClick={createNote}>Create Note</button>
       <div style={{marginBottom: 30}}>
-        {
-          notes.map(note => (
-            <div key={note.id || note.name}>
-              <h2>{note.name}</h2>
-              <p>{note.description}</p>
-              <button onClick={() => deleteNote(note)}>Delete note</button>
-            </div>
-          ))
-        }
+         {
+  notes.map(note => (
+    <div key={note.id || note.name}>
+      <h2>{note.name}</h2>
+      <p>{note.description}</p>
+      <button onClick={() => deleteNote(note)}>Delete note</button>
+      {
         note.image && <img src={note.image} style={{width: 400}} />
       }
     </div>
   ))
 }
+
       </div>
       <AmplifySignOut />
     </div>
-    
   );
 }
 
